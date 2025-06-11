9aeecd307c9142737aa2eb17a3fd44281db74faf
Ibne-suny <mdivnesuny2021@gmail.com>
2025-06-11 08:20:41 +0600

    print Z in java

---
+++ print_Z.java
@@ -0,0 +1,13 @@
+public class print_Z {
+    public static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("       * ");
+        System.out.println("      *");
+        System.out.println("     *");
+        System.out.println("    *");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("* * * * *");
+    }
+}
